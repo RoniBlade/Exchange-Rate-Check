a60da9665ad97a23c8784b76ffdc04e14cb5caf2
Adam Latyrov <adamlatyrov279@gmail.com>
2023-06-03 12:30:49 +0300

    fixcomms1

--- src/main/java/org/larbcorp/GifsApiService/GifApiService.java
+++
@@ -1,4 +0,0 @@
-package org.larbcorp.GifsApiService;
-
-public interface GifApiService {
-}
--- src/main/java/org/larbcorp/services/impl/MainServiceImpl.java
+++ src/main/java/org/larbcorp/services/impl/MainServiceImpl.java
@@ -1,36 +1,4 @@
 package org.larbcorp.services.impl;
 
-import lombok.RequiredArgsConstructor;
-import org.larbcorp.ExchangeRatesApiService.ExchangeRatesApiClient;
-import org.larbcorp.ExchangeRatesApiService.ExchangeRatesServiceImpl.ExchangeRatesServiceImpl;
-<<<<<<< HEAD
-=======
-import org.larbcorp.GifsApiService.GifApiServiceImpl.GifApiServiceImpl;
->>>>>>> c81b9e1334f64f8dc27099160d8de745db1e93db
-import org.larbcorp.models.ExchangeRate;
-import org.larbcorp.services.MainService;
-
-
-
-public class MainServiceImpl implements MainService {
-
-    ExchangeRatesServiceImpl exchangeRatesService = new ExchangeRatesServiceImpl();
-<<<<<<< HEAD
-=======
-    GifApiServiceImpl gifApiService = new GifApiServiceImpl();
->>>>>>> c81b9e1334f64f8dc27099160d8de745db1e93db
-    @Override
-    public String getRate(String curName) {
-
-        int value = exchangeRatesService.CurComparison(curName);
-
-<<<<<<< HEAD
-        return null;
-//        return LevMethodName(value);
-=======
-
-        return gifApiService.getGifUrl(value);
->>>>>>> c81b9e1334f64f8dc27099160d8de745db1e93db
-
-    }
+public class MainServiceImpl {
 }
