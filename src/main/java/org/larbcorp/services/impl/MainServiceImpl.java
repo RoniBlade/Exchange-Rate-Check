c81b9e1334f64f8dc27099160d8de745db1e93db
Лев <levostrov13@yandex.ru>
2023-06-03 11:34:34 +0300

    add getGifUrl

--- src/main/java/org/larbcorp/services/impl/MainServiceImpl.java
+++ src/main/java/org/larbcorp/services/impl/MainServiceImpl.java
@@ -3,6 +3,7 @@ package org.larbcorp.services.impl;
 import lombok.RequiredArgsConstructor;
 import org.larbcorp.ExchangeRatesApiService.ExchangeRatesApiClient;
 import org.larbcorp.ExchangeRatesApiService.ExchangeRatesServiceImpl.ExchangeRatesServiceImpl;
+import org.larbcorp.GifsApiService.GifApiServiceImpl.GifApiServiceImpl;
 import org.larbcorp.models.ExchangeRate;
 import org.larbcorp.services.MainService;
 
@@ -11,12 +12,14 @@ import org.larbcorp.services.MainService;
 public class MainServiceImpl implements MainService {
 
     ExchangeRatesServiceImpl exchangeRatesService = new ExchangeRatesServiceImpl();
+    GifApiServiceImpl gifApiService = new GifApiServiceImpl();
     @Override
     public String getRate(String curName) {
 
         int value = exchangeRatesService.CurComparison(curName);
 
-        return LevMethodName(value);
+
+        return gifApiService.getGifUrl(value);
 
     }
 }
