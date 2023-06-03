5c0841fd9c10607277dfe1c741f181e90cea61c6
Adam Latyrov <adamlatyrov279@gmail.com>
2023-06-03 13:01:37 +0300

    add method for compare today and yesterday currency

--- src/main/java/org/larbcorp/controllers/ExchangeRatesController.java
+++ src/main/java/org/larbcorp/controllers/ExchangeRatesController.java
@@ -1,35 +1,11 @@
 package org.larbcorp.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.larbcorp.models.ExchangeRate;
-import org.larbcorp.services.impl.MainServiceImpl;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-
-
-@Controller
 @RequiredArgsConstructor
 public class ExchangeRatesController {
 
-    @GetMapping("/")
-    public String showCurrencyForm(Model model) {
-        List<String> currencies = new ArrayList<>();
-        currencies.add("EUR");
-        currencies.add("RUB");
-        model.addAttribute("currencies", currencies);
-        return "index";
-    }
+    int a = 0;
 
-//    @PostMapping("/")
-//    public String showGif(@RequestParam("currency") String currencyCode, Model model) {
-//        // Здесь фримаркером передается ссылка на гифку в модель
-////        String gifUrl = getGifUrl(currencyCode); // Замените это соответствующим кодом для получения ссылки на гифку
-//        model.addAttribute("gifUrl", gifUrl);
-//        return "gif-page";
-//    }
 
 }
