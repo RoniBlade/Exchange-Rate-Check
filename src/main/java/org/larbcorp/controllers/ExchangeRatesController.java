d3da8bd90c05c0fb1eb84f6cfd2cbc55df036df6
Adam Latyrov <adamlatyrov279@gmail.com>
2023-06-03 11:53:32 +0300

    add files

--- src/main/java/org/larbcorp/controllers/ExchangeRatesController.java
+++ src/main/java/org/larbcorp/controllers/ExchangeRatesController.java
@@ -2,11 +2,9 @@ package org.larbcorp.controllers;
 
 import ch.qos.logback.core.model.Model;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
 import org.larbcorp.models.ExchangeRate;
 import org.larbcorp.services.impl.MainServiceImpl;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,17 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.List;
 
-=======
-import org.larbcorp.services.impl.MainServiceImpl;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
->>>>>>> c81b9e1334f64f8dc27099160d8de745db1e93db
+
+
 @Controller
 @RequiredArgsConstructor
 public class ExchangeRatesController {
 
-<<<<<<< HEAD
     @GetMapping("/")
     public String showCurrencyForm(Model model) {
         List<String> currencies = new ArrayList<>();
@@ -42,15 +36,4 @@ public class ExchangeRatesController {
 //        return "gif-page";
 //    }
 
-
-=======
-    private final MainServiceImpl usersService;
-
-    @GetMapping("/")
-    public String getUsersPage(Model model) {
-//        model.addAttribute("users", usersService.getAllRates());
-        return "Users/users_page";
-    }
-
->>>>>>> c81b9e1334f64f8dc27099160d8de745db1e93db
 }
