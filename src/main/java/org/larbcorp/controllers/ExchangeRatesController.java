4e12ad38f306152ef3c2ece936f43a8e89c98a52
Лев <levostrov13@yandex.ru>
2023-06-03 16:35:47 +0300

    comit vers 1.01, fix return correct giphy

--- src/main/java/org/larbcorp/controllers/ExchangeRatesController.java
+++ src/main/java/org/larbcorp/controllers/ExchangeRatesController.java
@@ -28,7 +28,7 @@ public class ExchangeRatesController {
 
         model.addAttribute("gifUrl", mainService.getUrl("EUR"));
 
-        <iframe src="https://giphy.com/embed/WISTaCYz1ZmiBs1iKm" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/Biteable-money-job-rich-WISTaCYz1ZmiBs1iKm">via GIPHY</a></p>
+       // <iframe src="https://giphy.com/embed/WISTaCYz1ZmiBs1iKm" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/Biteable-money-job-rich-WISTaCYz1ZmiBs1iKm">via GIPHY</a></p>
 
 
         return "index";
